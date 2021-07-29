f331e75387132f5ad805e054f796b7950dd5cbd3
Priyanka-Prajapati <contact.priyankaprajapati@gmail.com>
2021-07-29 14:29:44 +0530

    feat: updated file
    reading value of an input textarea into a console.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+var inputText =document.querySelector('#input-text');
 var btnTranslate = document.querySelector("#btn-translate");
 btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("You Clicked!")
+    console.log("You Clicked!");
+    console.log(inputText.value);
 })

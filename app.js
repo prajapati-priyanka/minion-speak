d0614edd68717b2dbb18b7748f4a2fafd5bc5ab4
Priyanka-Prajapati <contact.priyankaprajapati@gmail.com>
2021-07-31 12:50:28 +0530

    feat: functionality added

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 var inputText =document.querySelector('#input-text');
 var btnTranslate = document.querySelector("#btn-translate");
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("You Clicked!");
-    console.log(inputText.value);
-})
+var outputText = document.querySelector('#output-text');
+
+var serverURL ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+function urlContructor(inputText){
+    return serverURL+"?"+"text="+inputText;
+}
+
+function errorHandler(error){
+    console.log("fetch call failed");
+}
+
+
+function clickEventHandler(){
+    // console.log("clicked");
+    var inputTextContent = inputText.value;
+    // console.log(inputTextContent);
+    fetch(urlContructor(inputTextContent))
+        .then(response => response.json())
+        .then(data =>{
+            var textTranslated = data.contents.translated;
+            // console.log(textTranslated);
+            outputText.innerText = textTranslated;
+        })
+        .catch(errorHandler);
+
+
+
+    // outputText.innerText = inputTextContent;
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
